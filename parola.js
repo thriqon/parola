598a56075a03fd94a4a70ee8e5d8ae84d7bace3d
Jonas Weber <mail@jonasw.de>
2016-01-11 11:43:48 +0100

    refactor, 527

--- parola.js
+++ parola.js
@@ -1,63 +1,67 @@
 
-function isFunction(x) {
-  return typeof x == 'function';
-}
-
-function resolve(d, r, q, p, x,then,k) {
-  function notedReject(ttt, e) {
-    if (!k) {
-      ttt(e);
-      k = 1;
-    }
-  }
-  try {
-    x = isFunction(this) ? this(x) : x;
-    if (p == x) {
-      0();
-    } else if (x && x === Object(x) && isFunction((then = x.then))) {
-      then.call(x, notedReject.bind(0, resolve.bind(0,d,r,q,p)), notedReject.bind(0, q));
-    } else {
-      r(x);
-    }
-  } catch (e) {
-    notedReject(q, e);
-  }
-}
 
 exports.deferred = function deferred() {
-  var a = [], b = [];
+  function isFunction(x,a) {
+    return (typeof x == 'function'?x:a);
+  }
+
+  var fs = [], b;
   var mode;
 
-  function runLater(f) {
-    setTimeout(f, 0, b);
-  }
-
-  function setModeAndValue(nm, val) {
-    if (!mode) {
-      b = val;
-      nm.forEach(runLater);
-      mode = nm;
+  function handle(cbs) {
+    if (mode) {
+      setTimeout(cbs[mode], 0, b);
+    } else {
+      fs.push(cbs);
     }
   }
+
+  function setModeAndValue(val) {
+    if (!mode) {
+      b = val;
+      mode = this;
+      fs.forEach(handle);
+    }
+  }
+  var resolve1 = setModeAndValue.bind(1);
+  var reject1 = setModeAndValue.bind(2);
+
   return {
     promise:  {
       then: function then(onF, onR) {
         var d = deferred();
         var r = d.resolve, q = d.reject, p = d.promise;
 
-        onF = isFunction(onF) ? resolve.bind(onF, d, r, q, p) : r;
-        onR = isFunction(onR) ? resolve.bind(onR, d, r, q, p) : q;
-        if (!mode) {
-          a.push(onF);
-          b.push(onR);
-        } else {
-          runLater(mode == a ? onF : onR);
+        function resolve(l, x,then,k) {
+          function notedReject(ttt, e) {
+            if (!k) {
+              ttt(e);
+              k = 1;
+            }
+          }
+          try {
+            x = l ? l(x) : x;
+            if (p == x) {
+              0();
+            } else if (x && x === Object(x) && isFunction((then = x.then))) {
+              then.call(x, notedReject.bind(0, resolve.bind(0, 0)), notedReject.bind(0, q));
+            } else {
+              r(x);
+            }
+          } catch (e) {
+            notedReject(q, e);
+          }
         }
+
+        handle([0,
+          resolve.bind(0, isFunction(onF, r)),
+          resolve.bind(0, isFunction(onR, q))
+        ]);
         return p;
       }
     },
-    resolve: setModeAndValue.bind(0, a),
-    reject: setModeAndValue.bind(0, b)
+    resolve: resolve1,
+    reject: reject1,
   };
 };
 
